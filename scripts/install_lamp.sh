2ad9c31586e0eff5099d71b98cdb0ba8de1bb228
root <root@ip-172-31-92-247.ec2.internal>
2026-09-17 14:55:03 +0000

    Add Dockerfiles, installation scripts, and ECS configs

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Manual LAMP Installation Commands Used on Amazon Linux 2023
 sudo dnf update -y
 sudo dnf install -y httpd mariadb105-server php php-mysqlnd php-gd php-xml
 sudo systemctl start httpd mariadb
